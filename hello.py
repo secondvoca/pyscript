1b66432ed7b16c4af33a50b9c43c48bc30e1df79
zisei <secondvoca@gmail.com>
2022-08-07 14:08:25 +0900

    problems

---
+++ hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# Purpose: Say hello
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Say hello')
+parser.add_argument('-n',
+                    '--name',
+                    metavar='name',
+                    default='World',
+                    help='Name to greet')
+args = parser.parse_args()
+print(f'Hello, {args.name}!')
